94498bed3534151c30bd20f26a8456a5ba3cfda0
Awilliams4321 <awilliams1234@live.com>
2021-02-22 17:40:25 -0700

    Replacing my console.log with a dispatch action with a type of FETCH_TRAILS and a payload of trails. That way when it is sent to my trailsReducer, in the case of FETCH_TRAILS, it will return the payload from that action.

--- src/actions/trailsActions.js
+++ src/actions/trailsActions.js
@@ -2,6 +2,6 @@ export const fetchTrails = () => {
     return (dispatch) => {
         fetch("http://localhost:3000/trails")
         .then(resp => resp.json())
-        .then(trails => console.log('fetchTrails', trails))
+        .then(trails => dispatch({type: 'FETCH_TRAILS', payload: trails}))
     }
 }
