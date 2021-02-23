e20320015c46f2bd939e430829982463e5d6eddb
Awilliams4321 <awilliams1234@live.com>
2021-02-22 20:04:24 -0700

    Adding another case for ADD_TRAILS which will return a copy of all existing state and add a new trai(with action.payload).

--- src/reducers/trailsReducer.js
+++ src/reducers/trailsReducer.js
@@ -2,6 +2,8 @@ export const trailsReducer = (state = [], action) => {
     switch(action.type){
         case 'FETCH_TRAILS':
             return action.payload
+        case 'ADD_TRAIL':
+            return [...state, action.payload]    
         default:
             return state
     }
