f06c4db3b91c0e2c71003a6609c8e3fe17a4edd6
Awilliams4321 <awilliams1234@live.com>
2021-02-22 17:07:16 -0700

    Importing connect from redux.

---
+++ src/components/TrailsComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'redux'
+
+class TrailsComponent extends Component {
+    render() {
+        return (
+            <div>
+                Trails Container
+            </div>
+        );
+    }
+}
+
+export default TrailsComponent;
