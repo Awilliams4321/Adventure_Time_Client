553bd4261cb0be0e362b2d42b44f36b1de7ae8af
Awilliams4321 <awilliams1234@live.com>
2021-02-23 17:25:27 -0700

    Importing my Trails component and creating a /trails route for it.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import Homepage from './components/Homepage'
+import Homepage from '../components/Homepage'
+import TrailsComponent from '../components/TrailsComponent'
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path='/' component={Homepage} />
+            <Route path='/trails' component={TrailsComponent} />
+
         </Switch>
 
     );
