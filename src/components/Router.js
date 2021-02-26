14149cceb178623f8fe4514629a655d4e081c434
Awilliams4321 <awilliams1234@live.com>
2021-02-25 21:16:13 -0700

    Importing my Homepage component which my root route will be directed to..

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from '../components/Homepage'
-import TrailsComponent from '../components/TrailsComponent'
+import TrailsList from '../components/TrailsList'
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path='/' component={Homepage} />
-            <Route path='/trails' component={TrailsComponent} />
+            <Route path='/trails' component={TrailsList} />
 
         </Switch>
 
