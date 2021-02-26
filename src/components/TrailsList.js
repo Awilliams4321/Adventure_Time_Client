4a09198f9c4d601cdd86c1b6b33192d780750de0
Awilliams4321 <awilliams1234@live.com>
2021-02-25 21:09:54 -0700

    Importing the TrailsList to my Homepage.

--- src/components/TrailsList.js
+++ src/components/TrailsList.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
-class TrailsList extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+const TrailsList =({ trails }) => {
+    return (
+        <div>
+            {trails.map(trail => <ul><li key={trail.id}>
+                {trail.name} - {trail.location}<br/>
+                Length: {trail.length}<br/>
+                Est. Time: {trail.estimated_time}<br/>
+                Intensity: {trail.intensity}<br/>
+                </li></ul>)}
+        </div>
+    )
 }
 
 const mapStateToProps = state => {
