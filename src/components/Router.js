9d00337cd74480cd611e1678d53ff713997d2aed
Awilliams4321 <awilliams1234@live.com>
2021-03-01 09:05:14 -0700

    Adding an exact path for trails/new that links to the Trails Form component.

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from '../components/Homepage'
 import TrailsList from '../components/TrailsList'
+import TrailsForm from '../components/TrailsForm'
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path='/' component={Homepage} />
-            <Route path='/trails' component={TrailsList} />
+            <Route exact path='/trails' component={TrailsList} />
+            <Route exact path='/trails/new' component={TrailsForm} />
 
         </Switch>
 
