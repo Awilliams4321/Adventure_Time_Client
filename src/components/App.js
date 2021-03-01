9ad37b047685f3448771900e8be2dd27ec6cf4e0
Awilliams4321 <awilliams1234@live.com>
2021-03-01 09:46:30 -0700

    Importing and mounting my NavBar component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Router from './Router'
 import TrailsContainer from './TrailsContainer'
+import NavBar from './NavBar'
 
 const App = () => {
     return (
         <div>
+            <NavBar />
             <TrailsContainer />
             <Router />
         </div>
