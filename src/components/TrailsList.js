c4baf8f8cf810146a6b03c5b920716e882979b41
Awilliams4321 <awilliams1234@live.com>
2021-03-24 08:07:07 -0600

    Connecting the action deleteTrail to my component to be used in my delete button. Adding a delete button and add to favorite button in my mapped over array for each trail. Creating functions to handle the onClick events. I have successfully created my delete button. Handle favorites is still being worked on.

--- src/components/TrailsList.js
+++ src/components/TrailsList.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import deleteTrail from '../actions/trailsActions'
+import updateFavorites from '../actions/favoritesActions'
 
-const TrailsList =({ trails }) => {
+
+const TrailsList =({ trails, deleteTrail, history }) => {
+    const handleClick = event => {
+        const id = parseInt(event.target.id)
+        // debugger
+        // console.log("clicked", id )
+        deleteTrail(id)
+        history.push('/trails')
+    }
+    
+    const handleFavorites = event => {
+        event.preventDefault()
+        const favValue = !event.target.value
+        // !favValue
+        debugger
+        console.log("favorited")
+        // updateFavorites()
+    }
+
     return (
         <div>
             {trails.map(trail => <ul><li key={trail.id}>
+                {console.log(trails)}
                 {trail.name} - {trail.location}<br/>
                 Length: {trail.length}<br/>
                 Est. Time: {trail.estimated_time}<br/>
                 Intensity: {trail.intensity}<br/> 
+                {trail.favorite}
+
+                <Button><button id={trail.id} onClick={handleClick}>Delete Trail</button></Button>
+                <button id={trail.id} value={trail.favorite} onClick={handleFavorites}>Add to Favorites</button>
+
                 </li></ul>)}
+                
         </div>
     )
 }
 
 const mapStateToProps = state => {
-    return {trails: state.trails}
+    return {
+        trails: state.trails
+    }
 }
 
-export default connect(mapStateToProps)(TrailsList);
+export default connect(mapStateToProps, { deleteTrail })(TrailsList);
