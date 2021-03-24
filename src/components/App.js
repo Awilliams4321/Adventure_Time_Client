675f45361aaae24163eb25a986d1f59a1ddebcad
Awilliams4321 <awilliams1234@live.com>
2021-03-24 07:53:18 -0600

    Adding files for styling purposes.

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Router from './Router'
 import TrailsContainer from './TrailsContainer'
 import NavBar from './NavBar'
+import DeleteButton from './Like';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
     return (
         <div>
+            <h1>Adventure Time!</h1>
             <NavBar />
             <TrailsContainer />
             <Router />
