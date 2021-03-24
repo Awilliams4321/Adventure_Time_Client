741df420ddbcc3dafba07ba3d0523de971682ea8
Awilliams4321 <awilliams1234@live.com>
2021-03-24 07:58:53 -0600

    Creating a link for Favorite Trails.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function NavBar(props){
+export default function NavBar(){
     return (
         <nav>
             <Link to="/trails">All Trails</Link>
             <Link to="/trails/new">New Trail</Link>
+            <Link to="/trails/favorites">Favorite Trails</Link>
         </nav>
     )
 }
