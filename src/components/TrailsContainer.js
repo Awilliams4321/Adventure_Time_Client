d34ff20eabdc7d04ea0495cd821b0a0cbb23fc38
Awilliams4321 <awilliams1234@live.com>
2021-04-02 10:21:44 -0600

    Wrapping my TrailPage component in Route Component to allow a '/trails path to be created for it.

--- src/components/TrailsContainer.js
+++ src/components/TrailsContainer.js
@@ -10,16 +10,6 @@ import updateFavorites from '../actions/favoritesActions'
 import { Button } from 'reactstrap';
 
 class TrailsContainer extends Component {
-
-    // state = {
-    //     count: 0
-    // }
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {count: 0};
-    
-    //     this.handleCounter = this.handleCounter.bind(this);
-    // }
  
     componentDidMount() {
         this.props.fetchTrails()
@@ -36,13 +26,6 @@ class TrailsContainer extends Component {
             deleteTrail,
             history
         } = this.props;
-
-        // const handleClick = event => {
-        //     const id = parseInt(event.target.id)
-        //     deleteTrail(id)
-        //     // console.log(history)
-        //     history.push('/trails')
-        // }
         
         // const handleFavorites = event => {
         //     event.preventDefault()
@@ -52,22 +35,12 @@ class TrailsContainer extends Component {
         //     // console.log("favorited")
         //     // updateFavorites(favValue)
         // }
-
-        // const handleCounter = e => {
-        //     // debugger
-        //     const buttonNum = parseInt(e.target.value)
-        //     debugger
-        //     this.setState(state => ({
-        //         count: parseInt(e.target.value) + 1
-        //     }))
-           
-        //     // console.log(this.state.count + 1)
-           
-        // }
         
         return (
             <ul>
+                <Route exact path="/trails">
                 {this.props.trails.map(trail => <TrailPage trail={trail}/>)}
+                </Route>
             </ul>
             
         );
@@ -77,7 +50,6 @@ class TrailsContainer extends Component {
 const mapStateToProps = state => {
     return {
         trails: state.trails,
-        // favorites: state.trails.favorite
     }
 }
 
