358bbb0f529d104a08686c95724a7f2e6d51844c
Awilliams4321 <awilliams1234@live.com>
2021-04-02 13:00:28 -0600

    Commenting out my Routes file to see if my code works without it.

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import TrailsContainer from './TrailsContainer'
-// import TrailsPage from './TrailPage'
-import TrailsForm from './TrailsForm'
-import FavoriteTrailsPage from './FavoriteTrailsPage'
+// import React from 'react';
+// import { Switch, Route } from 'react-router-dom'
+// import TrailsContainer from './TrailsContainer'
+// // import TrailsPage from './TrailPage'
+// import TrailsForm from './TrailsForm'
+// import FavoriteTrailsPage from './FavoriteTrailsPage'
 
-const Router = () => {
-    return (
-        <Switch>
-            <Route exact path='/' component={TrailsContainer} />
-            <Route exact path='/trails' component={TrailsContainer} />
-            <Route exact path='/trails/new' component={TrailsForm} />
-            <Route exact path='/trails/favorites' component={FavoriteTrailsPage} />
+// const Router = () => {
+//     return (
+//         <Switch>
+//             <Route exact path='/' component={TrailsContainer} />
+//             <Route exact path='/trails' component={TrailsContainer} />
+//             <Route exact path='/trails/new' component={TrailsForm} />
+//             <Route exact path='/trails/favorites' component={FavoriteTrailsPage} />
 
-        </Switch>
+//         </Switch>
 
-    );
-};
+//     );
+// };
 
-export default Router;
+// export default Router;
