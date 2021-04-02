9dce0d720fa2e298bc277e536e19489dcdbcdbba
Awilliams4321 <awilliams1234@live.com>
2021-04-02 13:02:55 -0600

    Commenting out the Trail file entirely.

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -1,22 +1,22 @@
-import { connect } from 'react-redux'
-import deleteTrail from './actions/deleteTrail'
-import { withRouter } from 'react-router-dom'
-import TrailsForm from './TrailsForm'
+// import { connect } from 'react-redux'
+// import deleteTrail from './actions/deleteTrail'
+// import { withRouter } from 'react-router-dom'
+// import TrailsForm from './TrailsForm'
 
-function Trail(props){
+// function Trail(props){
     
-    const handleClick = () => {
-        props.deleteTrail(props.trail.id)
-        props.history.push('/trail')
+//     const handleClick = () => {
+//         props.deleteTrail(props.trail.id)
+//         props.history.push('/trail')
 
-    }
+//     }
 
-    return (
-    <>
-        <li>{props.list && props.trail.name}<button onClick={handleClick}>Delete</button></li>
-        <TrailsForm list={props.trail}/>
-    </>
-    )
-}
+//     return (
+//     <>
+//         <li>{props.list && props.trail.name}<button onClick={handleClick}>Delete</button></li>
+//         <TrailsForm list={props.trail}/>
+//     </>
+//     )
+// }
 
-export default withRouter(connect(null, { deleteTrail })(Trail))
+// export default withRouter(connect(null, { deleteTrail })(Trail))
