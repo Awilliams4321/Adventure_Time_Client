0729ac684cd4cd7801e746fd14b8588003c23eef
Awilliams4321 <awilliams1234@live.com>
2021-04-02 13:10:07 -0600

    Creating this as a container for my favorite trails.

---
+++ src/components/FavoriteTrailsPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux'
+// import { Switch, Route } from 'react-router-dom'
+import Favorites from './Favorites'
+
+
+class FavoriteTrailsPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <Favorites />
+            </div>
+            
+        );
+    }
+}
+
+export default FavoriteTrailsPage;
