5e86efd64c42a133d821c218888285596ea77f1d
Awilliams4321 <awilliams1234@live.com>
2021-04-01 21:21:18 -0600

    Commenting out this component because a new TrailsContainer component will be a duplicate.

--- src/components/TrailsList.js
+++ src/components/TrailsList.js
@@ -1,50 +1,55 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import deleteTrail from '../actions/trailsActions'
-import updateFavorites from '../actions/favoritesActions'
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux'
+// import deleteTrail from '../actions/trailsActions'
+// import updateFavorites from '../actions/favoritesActions'
+// import { Button } from 'reactstrap';
 
-
-const TrailsList =({ trails, deleteTrail, history }) => {
-    const handleClick = event => {
-        const id = parseInt(event.target.id)
-        // debugger
-        // console.log("clicked", id )
-        deleteTrail(id)
-        history.push('/trails')
-    }
+// const TrailsList =({ trails, deleteTrail, history }) => {
+//     // componentDidMount() {
+//     //     this.props.fetchTrails()
+//     // } 
     
-    const handleFavorites = event => {
-        event.preventDefault()
-        const favValue = !event.target.value
-        // !favValue
-        debugger
-        console.log("favorited")
-        // updateFavorites()
-    }
+//     const handleClick = event => {
+//         const id = parseInt(event.target.id)
+//         deleteTrail(id)
+//         console.log(history)
+//         history.push('/trails')
+//     }
+    
+//     const handleFavorites = event => {
+//         event.preventDefault()
+//         const favValue = event.target.value
+//         // favValue
+//         debugger
+//         // console.log("favorited")
+//         updateFavorites(favValue)
+//     }
 
-    return (
-        <div>
-            {trails.map(trail => <ul><li key={trail.id}>
-                {console.log(trails)}
-                {trail.name} - {trail.location}<br/>
-                Length: {trail.length}<br/>
-                Est. Time: {trail.estimated_time}<br/>
-                Intensity: {trail.intensity}<br/> 
-                {trail.favorite}
+//     return (
+//         <div>
+//             {trails.map(trail => <ul><li key={trail.id}>
+//                 {console.log(trails)}
+//                 {trail.name} - {trail.location}<br/>
+//                 Length: {trail.length}<br/>
+//                 Est. Time: {trail.estimated_time}<br/>
+//                 Intensity: {trail.intensity}<br/> 
+//                 {trail.favorite}
 
-                <Button><button id={trail.id} onClick={handleClick}>Delete Trail</button></Button>
-                <button id={trail.id} value={trail.favorite} onClick={handleFavorites}>Add to Favorites</button>
-
-                </li></ul>)}
                 
-        </div>
-    )
-}
+//                 <Button color="danger" id={trail.id} onClick={handleClick}>Delete Trail</Button>{' '}
+//                 <Button color="success" id={trail.id} value={trail.favorite} onClick={handleFavorites}>Add to Favorites</Button>
 
-const mapStateToProps = state => {
-    return {
-        trails: state.trails
-    }
-}
+//                 </li></ul>)}
+                
+//         </div>
+//     )
+// }
 
-export default connect(mapStateToProps, { deleteTrail })(TrailsList);
+// const mapStateToProps = state => {
+//     return {
+//         trails: state.trails,
+//         // favorites: state.trails.favorite
+//     }
+// }
+
+// export default connect(mapStateToProps, { deleteTrail, updateFavorites })(TrailsList);
