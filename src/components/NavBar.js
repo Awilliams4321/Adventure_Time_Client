f3df0b6fd6604311c6bd2615549f0cd1e483a564
Awilliams4321 <awilliams1234@live.com>
2021-04-01 21:15:55 -0600

    Adding spaces to the links.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function NavBar(){
     return (
         <nav>
-            <Link to="/trails">All Trails</Link>
-            <Link to="/trails/new">New Trail</Link>
+            <Link to="/trails">All Trails</Link>{' '}
+            <Link to="/trails/new">New Trail</Link>{' '}
             <Link to="/trails/favorites">Favorite Trails</Link>
         </nav>
     )
